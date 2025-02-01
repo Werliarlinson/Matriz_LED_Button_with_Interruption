0f24d28a8f0a069fb94a150593c8001474e0f8a6
Werliarlinson <riugen1@hotmail.com>
2025-02-01 12:06:46 -0300

    Parte final, com retirada dos buzzers, melhor organização e comentários

--- Matriz_LED_Button_with_Interruption.c
+++ Matriz_LED_Button_with_Interruption.c
@@ -1,38 +1,22 @@
-#include <stdio.h>
-#include "pico/stdlib.h"
-#include "hardware/pio.h"
-#include "hardware/clocks.h"
+#include <stdio.h>                  // Biblioteca padrão de entrada e saída
+#include "pico/stdlib.h"            // Biblioteca padrão para o Raspberry Pi Pico
+#include "hardware/pio.h"           // Biblioteca para manipulação de periféricos PIO
+#include "hardware/clocks.h"        // Biblioteca para controle de relógios do hardware
 #include "ws2818b.pio.h"            // Programa para controle da matriz de LEDs WS2812
-#include "hardware/timer.h"
-#include "hardware/pwm.h"
 
 #define LED_RGB 13                  // Pino GPIO conectado ao LED RGB (vermelho)
-
 #define LED_COUNT 25                // Número de LEDs na matriz
 #define LED_PIN 7                   // Pino GPIO conectado a matriz de LEDs
 
-static volatile uint32_t last_time = 0; // Armazena o tempo do último evento (em microssegundos)
-volatile uint32_t interval = 1000; // Intervalo inicial em milissegundos (1 segundos)
-static int32_t set_button = 0;
-static volatile bool set_anime = 0;
-volatile int combination_index = 0; // Índice da combinação atual
+static volatile uint32_t last_time = 0;     // Armazena o tempo do último evento (em microssegundos)
+static int32_t set_button = 0;              // Controlador de seleção das animações
+static volatile bool set_anime = 0;         // Controlador de seleção do tipo de animações
+volatile int combination_index = 0;         // Índice da combinação atual
 
-// Matriz de combinações de tempos para as notas (em microssegundos)
-const uint32_t note_combinations[9][4] = {
-    {1000, 1000, 1000, 1000}, // 1 nota por segundo
-    {500, 500, 500, 500},     // 1 nota a cada 0,5 segundos
-    {250, 500, 500, 1000},   // 1 nota de 1s e 2 de 0,5s
-    {500, 1000, 500, 1000},   // 1 nota de 0,5s e 2 de 1s
-    {500, 500, 1000, 1000},   // 2 notas de 0,5s e 2 de 1s
-    {1000, 1000, 500, 500},   // 2 notas de 1s e 2 de 0,5s
-    {500, 1000, 1000, 500},   // 1 nota de 0,5s, 2 de 1s e 1 de 0,5s
-    {1000, 500, 1000, 500},   // 1 nota de 1s, 1 de 0,5s, 1 de 1s e 1 de 0,5s
-    {500, 500, 500, 500}      // 4 notas de 0,5s
-};
+int led_count = 0;                      // Contador do led
 
-const uint buzzer_pin = 10;         // GPIO do buzzer
-const uint button_A = 5;            // GPIO do botão A
-const uint button_B = 6;            // GPIO do botão B
+const uint button_A = 5;             // GPIO do botão A
+const uint button_B = 6;             // GPIO do botão B
 const uint button_joy = 22;          // GPIO do botão joystick
 
 struct pixel_t { 
@@ -61,19 +45,19 @@ int getIndex(int x, int y) {
 // Função para inicializar o PIO para controle dos LEDs
 void npInit(uint pin) 
 {
-    uint offset = pio_add_program(pio0, &ws2818b_program); // Carregar o programa PIO
-    np_pio = pio0;                                         // Usar o primeiro bloco PIO
+    uint offset = pio_add_program(pio0, &ws2818b_program);      // Carregar o programa PIO
+    np_pio = pio0;                                              // Usar o primeiro bloco PIO
 
-    sm = pio_claim_unused_sm(np_pio, false);              // Tentar usar uma state machine do pio0
-    if (sm < 0)                                           // Se não houver disponível no pio0
+    sm = pio_claim_unused_sm(np_pio, false);                    // Tentar usar uma state machine do pio0
+    if (sm < 0)                                                 // Se não houver disponível no pio0
     {
-        np_pio = pio1;                                    // Mudar para o pio1
-        sm = pio_claim_unused_sm(np_pio, true);           // Usar uma state machine do pio1
+        np_pio = pio1;                                          // Mudar para o pio1
+        sm = pio_claim_unused_sm(np_pio, true);                 // Usar uma state machine do pio1
     }
 
-    ws2818b_program_init(np_pio, sm, offset, pin, 800000.f); // Inicializar state machine para LEDs
+    ws2818b_program_init(np_pio, sm, offset, pin, 800000.f);    // Inicializar state machine para LEDs
 
-    for (uint i = 0; i < LED_COUNT; ++i)                  // Inicializar todos os LEDs como apagados
+    for (uint i = 0; i < LED_COUNT; ++i)                        // Inicializar todos os LEDs como apagados
     {
         leds[i].R = 0;
         leds[i].G = 0;
@@ -99,7 +83,7 @@ void npClear()
 // Função para atualizar os LEDs no hardware
 void npWrite() 
 {
-    for (uint i = 0; i < LED_COUNT; ++i)                  // Iterar sobre todos os LEDs
+    for (uint i = 0; i < LED_COUNT; ++i)                      // Iterar sobre todos os LEDs
     {
         pio_sm_put_blocking(np_pio, sm, leds[i].G<<24);       // Enviar componente verde
         pio_sm_put_blocking(np_pio, sm, leds[i].R<<24);       // Enviar componente vermelho
@@ -107,33 +91,7 @@ void npWrite()
     }
 }
 
-//função para inicializar o buzzer
-void pico_buzzer_init(uint gpio) {
-    gpio_set_function(gpio, GPIO_FUNC_PWM);
-    uint slice_num = pwm_gpio_to_slice_num(gpio);
-    pwm_set_enabled(slice_num, true);
-}
-
-//função para tocar uma nota no buzzer
-void pico_buzzer_play(uint gpio, uint frequency) {
-    uint slice_num = pwm_gpio_to_slice_num(gpio);
-    uint32_t clock = 125000000; 
-    uint32_t divider = clock / (frequency * 4096); 
-    uint32_t wrap = (clock / divider) / frequency - 1;
-    uint32_t level = wrap / 2; 
-    pwm_set_clkdiv(slice_num, divider);
-    pwm_set_wrap(slice_num, wrap);
-    pwm_set_chan_level(slice_num, PWM_CHAN_A, level);
-    pwm_set_enabled(slice_num, true);
-}
-
-//função para parar o buzzer
-void pico_buzzer_stop(uint gpio) {
-    uint slice_num = pwm_gpio_to_slice_num(gpio);
-    pwm_set_chan_level(slice_num, PWM_CHAN_A, 0);
-    pwm_set_enabled(slice_num, false);
-}
-
+// Função para imprimir um frame na matriz de LEDs de maneira padronizada e sem dificuldades
 void print_frame(int frame[5][5][3])
 {
     for(int linha = 0; linha < 5; linha++){
@@ -146,6 +104,7 @@ void print_frame(int frame[5][5][3])
     
 }
 
+// Função para imprimir a animação dos números árabicos
 void animation_number_ara(int number){
 
     //Lista dos numeros arabicos
@@ -235,6 +194,7 @@ void animation_number_ara(int number){
 
 }
 
+// Função para imprimir a animação dos números poligonais
 void animation_number_pol(int number){
 
     //Lista dos numeros em poligonos
@@ -326,60 +286,53 @@ void animation_number_pol(int number){
 // Função para lidar com a interrupção dos botões
 void gpio_irq_handler(uint gpio, uint32_t events) {
     
-    uint32_t current_time = to_us_since_boot(get_absolute_time()); // Obter o tempo atual em microssegundos
+    uint32_t current_time = to_us_since_boot(get_absolute_time());      // Obter o tempo atual em microssegundos
 
-    if(current_time - last_time > 300000) { // Ignorar eventos muito próximos
+    if(current_time - last_time > 300000) {                             // Ignorar eventos muito próximos
        
         last_time = current_time;
         
-        if (gpio == button_B) {
-            combination_index = (set_button + 1) % 9; // Avança para a próxima combinação
+        if (gpio == button_B) {                                         // Avança para a próxima combinação
             set_button++;
             if(set_button > 9){
                 set_button = 9;
             } 
         } 
-        if (gpio == button_A) {
-            combination_index = (set_button - 1) % 9; // Retrocede para a combinação anterior
+        if (gpio == button_A) {                                         // Retrocede para a combinação anterior
             set_button--;
             if(set_button < 0){
                 set_button = 0;
             }
         }
         if (gpio == button_joy) {
-            set_anime = !set_anime;
+            set_anime = !set_anime;                                     // Alterna entre os tipos de animações
         }
     
     }
 
 }
 
+// Função para executar a lógica principal do programa
 void loop_playground() {
-    
-    static int beat = 0;
-   
-    pico_buzzer_play(buzzer_pin, 291); // Toca uma nota grave
+
     gpio_put(LED_RGB, 1);                             // Acender o LED RGB
     sleep_ms(100);                                    // Aguardar 100ms
     gpio_put(LED_RGB, 0);                             // Apagar o LED RGB
     sleep_ms(100);                                    // Aguardar 100ms
-    if(beat == note_combinations[combination_index][beat]) {
-    pico_buzzer_stop(buzzer_pin);
-    }
-    beat = note_combinations; // Avançar para o próximo tempo
+    led_count++;                                      // Incrementar o contador de acionamentos do LED RGB
     switch (set_anime) {
         case 0: animation_number_ara(set_button); break;
         case 1: animation_number_pol(set_button); break;
     }
+    
 }
 
 int main()
 {
     //Inicialização dos periféricos
+    stdio_init_all();                                     // Inicializar a comunicação serial
     gpio_init(LED_RGB);                                   // Inicializar o pino do LED RGB
     gpio_set_dir(LED_RGB, GPIO_OUT);                      // Configurar o pino do LED RGB como saída
-    
-    pico_buzzer_init(buzzer_pin);                         // Inicializar o buzzer
     
     gpio_init(button_A);                                  // Inicializar o pino do botão A
     gpio_set_dir(button_A, GPIO_IN);                      // Configurar o pino do botão A como entrada
@@ -404,6 +357,15 @@ int main()
 
     while (true) {
         loop_playground();
+        
+        // Verificar a contagem de acionamentos do LED a cada segundo
+        static uint32_t last_time = 0;
+        uint32_t current_time = to_ms_since_boot(get_absolute_time());
+        if (current_time - last_time >= 1000) {         // Verifica se passou 1 segundo
+            printf("O LED acendeu %d vezes no último segundo.\n", led_count);
+            led_count = 0;                              // Reseta a contagem
+            last_time = current_time;
+        }
     }
     
     return 0;
